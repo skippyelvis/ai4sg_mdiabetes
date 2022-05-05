no duplicate messages, average rewards, fixed memory

mdiabetes.py
@@ -67,15 +67,12 @@ class MDiabetes:
         prev_actions, prev_clusters, responses = self.collect_responses()
         MainLogger("updating states and adding transitions")
         next_states, transitions = self.update_states(prev_actions, prev_clusters, responses, states, ids)
-        for r in transitions:
-            print(r[1])
-            print(r[3])
         if self.run_index > 1 and len(transitions) > 0:
             weekly_loss, cluster_t_counts = self.agent.weekly_training_update(transitions, self.run_index)
         MainLogger("analyzing ai performance")
         debug = debugai(ai, actions, ai_random, weekly_loss, ids, \
                 clusters, states, cluster_debug, cluster_t_counts)
-        MainLogger(cluster_t_counts)
+        MainLogger("transitions/cluster: ", cluster_t_counts)
         if not self.dry_run:
             MainLogger("Saving data")
             self.stor["states"].save_data(next_states, self.run_index)
@@ -91,7 +88,7 @@ class MDiabetes:
             Path("dry_run_check").mkdir(exist_ok=True)
             MQDryStor.save_data(msg_qsn, self.run_index)
             DBDryStor.save_data(debug, self.run_index)
-        MainLogger("="*20)
+        MainLogger("="*5, "SUCCESS", "="*5)
 
     def gather_participants(self):
         # load previous timeline, ids, and states
@@ -264,6 +261,7 @@ class MDiabetes:
         resp = self.stor["responses"].load_indexed_data(self.run_index-1)
         cresp = None
         if prev_actions is not None and resp is not None:
+            MainLogger("handling responses")
             cresp = torch.zeros(prev_actions.size(0), 5).long()
             if len(resp) == 0:
                 return None, None
@@ -290,16 +288,20 @@ class MDiabetes:
             update_idxs[i] = (ids == actions[i,0]).nonzero()[0]
         rewards = torch.zeros(actions.size(0)).long()
         for i, idx in enumerate(update_idxs):
+            # TODO: duplicate elem --> max(reward), otherise --> average
             state = states[idx]
             action = actions[i,1]
+            crewards = [None, None]
             reward = 0
             next_state = next_states[idx].clone()
             resp = responses[i, [2,4]]
             sid = torch.tensor(MessagesH.sid_lookup(action)).long()
+            duplicate_elem = sid[0] == sid[1]
             for j in range(resp.size(0)):
-                reward += torch.clip(resp[j] - state[sid[j]-1], 0, 3)
+                crewards[j] = torch.clip(resp[j] - state[sid[j]-1], 0, 3)
                 next_state[sid[j]-1] = resp[j]
             next_states[idx] = next_state
+            reward = sum(crewards)/len(crewards)
             rewards[i] = reward
             if resp.sum() == 0:
                 continue

model.py
@@ -170,7 +170,7 @@ class DQN:
         cv = self.convergence[train_or_warmup]
         return ConvergenceCheck(**cv)
 
-    def weekly_training_update(self, transitions, run_index):
+    def weekly_training_update(self, cn, transitions, run_index):
         self.memory.add(transitions)
         lossh = torch.tensor([])
         if self.memory.N == 0:
@@ -179,9 +179,9 @@ class DQN:
         DQNLogger("Starting training", btbrk=None)
         optimizer = self.new_optimizer(self.train_lr)
         for ns in range(self.num_samples):
-            sample_lossh, sample_reward = self.train_on_sample(optimizer)
             if ns == 0:
-                print(sample_reward)
+                self.check_errors(cn)
+            sample_lossh = self.train_on_sample(optimizer)
             if sample_lossh is None:
                 break
             lossh = torch.cat((lossh, sample_lossh))
@@ -200,19 +200,21 @@ class DQN:
         DQNLogger("Done training", topbrk=None)
         return lossh
     
-    def check_errors(self):
+    def check_errors(self, cn):
         sample = self.memory.mem
         if sample is None:
             return None
         lossh = []
-        policy_qvals = self.policy(sample['state']).gather(1, sample['action'])
-        target_reward = sample['reward']
+        with torch.no_grad():
+            policy_qvals = self.policy(sample['state']).gather(1, sample['action'])
+        target_reward = sample['reward'].clone()
         future = self.target.predict(sample['next_state'])
         future = self.gamma * future.max(1)[0].reshape(-1,1)
         target_reward += future
-        err = policy_qvals - target_reward
-        # l = torch.cat((policy_qvals, target_reward, sample['reward']),1)
-        # print(l)
+        l = torch.cat((policy_qvals, target_reward, sample['reward']),1)
+        import os
+        os.makedirs("check_memory_errors", exist_ok=True)
+        torch.save(l, "check_memory_errors/" + str(cn)+".pt")
     
     def train_on_sample(self, optimizer):
         sample = self.memory.sample(self.dev)
@@ -222,7 +224,7 @@ class DQN:
         criterion = self.new_criterion()
         optimizer.zero_grad()
         policy_qvals = self.policy(sample['state']).gather(1, sample['action'])
-        target_reward = sample['reward']
+        target_reward = sample['reward'].clone()
         future = self.target.predict(sample['next_state'])
         future = self.gamma * future.max(1)[0].reshape(-1,1)
         target_reward += future
@@ -231,7 +233,7 @@ class DQN:
         self.policy.clamp_grads()
         optimizer.step()
         lossh.append(loss.item())
-        return torch.tensor(lossh), sample['reward'] 
+        return torch.tensor(lossh) 
 
     def train_warmup(self, warmup_states, warmup_targets):
         warmup_targets = warmup_targets.to(self.dev)
@@ -263,10 +265,16 @@ class DQN:
         return torch.tensor(lossh)
 
     def choose_action_from_qvals(self, q, samp=0.025):
-        asort = q.argsort(descending=True).flatten()
-        qf = q.flatten()
-        k = int((samp * N_ACTION) +1)
-        candidates = asort[:k]
+        # randomly select an action from the top samp% of q
+        asort = q.argsort(descending=True).flatten()  # argsort --> indexes
+        duplicate_mask = torch.zeros_like(asort).bool()
+        i = 0
+        for a in asort:
+            duplicate_mask[i] = MessagesH.duplicate_sid(a).item()
+            i += 1
+        asort = asort[~duplicate_mask]
+        k = int((samp * asort.shape[0]) +1)                 # cutoff index
+        candidates = asort[:k]                        # sample action slice
         choice = torch.randint(k, (1,))
         select_pred = candidates[choice]
         return select_pred